1b9e44a0683dd78c6ea4519fcde7f648c86234ef
Shakir <em.shakir@outlook.com>
2023-06-26 22:05:57 +0530

    Initial commit

---
+++ src/main/scala/lectures/basics/$01_ValuesVariablesTypes.scala
@@ -0,0 +1,46 @@
+package lectures.basics
+
+object $01_ValuesVariablesTypes extends App {
+
+  val x: Int = 42
+  println(x)
+
+  // Values are Immutable(constants) in scala, so if we re-initialize them, compiler will complain
+  //x = 50
+
+  // Compiler can Infer the Type based on assigned value so no need to specifically mention type
+  val anString: String = "Hello Scala!"
+
+  // Characters
+  val aString = "Hello Scala!"
+  val aCharacter: Char = 'a'
+
+  // Boolean
+  val aBoolean: Boolean = true
+
+  // Numbers
+  val anInteger: Int = 50
+  val aShort: Short = 5555
+  val aLong: Long = 4444444455L
+
+  //Decimals
+  val aFloat: Float = 52f
+  val aDouble: Double = 458.524
+
+  // Variables: Which are Mutable we can re-initialize a Variable which can also be called side-effects
+
+  var aVarInt: Int = 88
+  aVarInt = 99
+
+
+  /*
+    - Prefer vals Over vars
+    - all vals and vars have Types
+    - Compiler automatically infer types when omitted
+    - Basic Types:
+      - Boolean
+      - Int, Short, Long
+      - Float, Double
+      - String, Char
+   */
+}
---
+++ src/main/scala/lectures/basics/$02_Expressions.scala
@@ -0,0 +1,75 @@
+package lectures.basics
+
+object $02_Expressions extends App {
+
+  val x = 1 + 1 //Expressions
+  println(x);
+
+  // + - * / & | ^ << >> >>>(right shift with zero extension)
+  println(1 + 2 * 3)
+
+  // == != > >= < <=
+  println(1 == x)
+
+  // ! && ||
+  println(!(1 == x))
+
+  var aVariable = 2
+  aVariable += 3
+
+  // also works with -=, *=, /= ...... Side Effects
+  println(aVariable)
+
+  // Instructions (DO Something) vs Expressions( return something)
+
+  val aCondition = true
+  val aConditionValue = if (aCondition) 5 else 3;
+  println(aConditionValue)
+
+  // Everything in Scala is Expression(Give Something)
+
+  val aWeirdValue = (aVariable = 5) // Unit == void in  Scala
+  println(aWeirdValue)
+
+  // Side Effects -> println(), whiles, reassigning..
+
+  // Code Blocks: Special Expressions
+
+  val aCodeBlock = {
+    val y = 2
+    val z = y + 1
+
+    if (z > 2) "Hello" else "Good Bye"
+  }
+
+  val someValue = {
+    2 < 3
+  }
+  println(someValue) // true
+
+  val someOtherValue = {
+    if (someValue) 239 else 986
+    45
+  }
+  println(someOtherValue) // 45
+
+}
+
+/*
+  TAKE AWAYS
+      - Code Blocks in Scala are Expressions
+      - The Value of the bock is the value of its last Expression
+
+      Expression vs Instructions
+        - Instructions are Executed(Java), Expression are evaluated(Scala)
+        - In Scala we'll think in terms of expression
+
+      NOTE: Do not use while loop in Scala Programs
+
+
+  EXERCISE
+
+      1. Difference between "Hello World" vs println("Hello World")
+      A:    - Hello World is Value of type String - literal returns String
+            - println("Hello World") is expression(side effect) returns Unit
+ */
